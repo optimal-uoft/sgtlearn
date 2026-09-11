9fa303d2f1d7b3affa56814b4523c44ff835dce1
Nakul Upadhya <nakulupadhya1@gmail.com>
2026-09-11 13:31:41 -0400

    Release v0.3.1 with refreshed tutorial outputs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ try:
 
     release = _pkg_version("sgtlearn")
 except Exception:  # noqa: BLE001 - fall back to a hardcoded value off-RTD
-    release = "0.3.0"
+    release = "0.3.1"
 version = ".".join(release.split(".")[:2])
 
 # -- Project information -----------------------------------------------------
